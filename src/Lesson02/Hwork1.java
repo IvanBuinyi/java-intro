d20892c1d54bc042b8ce48c4a8a0133a422ae3f4
Ivan.Buinyi <buiniy.ivan@gmail.com>
2019-02-26 01:15:15 +0200

    HomeWork Lesson 2 Commit 1

---
+++ src/Lesson02/Hwork1.java
@@ -0,0 +1,12 @@
+package Lesson02;
+
+public class Hwork1 {
+    public static void main(String[] args) {
+        int n = 2;
+        String eachNth = new String ("asdfghj");
+        char result1 = eachNth.charAt(n);
+
+        System.out.println("The " + n +"th of " + eachNth + " is " + result1);
+
+    }
+}
