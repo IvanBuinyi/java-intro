e5adbae6c0d51baf75c9e851129af4af0041a551
Ivan.Buinyi <buiniy.ivan@gmail.com>
2019-02-27 19:43:46 +0200

    HomeWork Lesson 2 Commit 2

--- src/Lesson02/Hwork1.java
+++ src/Lesson02/Hwork1.java
@@ -2,18 +2,14 @@ package Lesson02;
 
 public class Hwork1 {
 
-    private static String s;
-    private static int n = 3;
-
-
-
-        {
-            String s = "Some text for test";
-            for (int i = 0; i < s.length(); i= i+n) {
+    public Hwork1(String s, int n) {
+        String b = s;
+        if (b.length() == 0) System.out.println("Strint shouldn't be empty");
+        else {
+            for (int i = 0; i < s.length(); i = i + n)
                 System.out.println(s.charAt(i));
-            }
-
-
         }
-
+    }
 }
+
+
--- src/Lesson02/Programm2.java
+++ src/Lesson02/Programm2.java
@@ -2,11 +2,11 @@ package Lesson02;
 
 public class Programm2 {
     public static void main(String[] args) {
-        Person p1 = new Person("Bob", "Dilan",21);
-        Person p2 = new Person("Ivan", "Buinyi",12);
-        System.out.println(p1);
-        System.out.println(p2);
-
+        //Person p1 = new Person("Bob", "Dilan",21);
+        //Person p2 = new Person("Ivan", "Buinyi",12);
+        Hwork1  H1 = new Hwork1 ("Some text for home work test", 5);
+        //System.out.println(p1);
+        //System.out.println(p2);
         }
 
 }
