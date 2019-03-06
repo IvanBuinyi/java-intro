6a5dceb9e18a4614c097e5861103f8182d7129fc
Ivan.Buinyi <buiniy.ivan@gmail.com>
2019-03-06 20:02:48 +0200

    HomeWork Lesson 4 Commit 1

--- src/Lesson02/Hwork1.java
+++ src/Lesson02/Hwork1.java
@@ -2,13 +2,16 @@ package Lesson02;
 
 public class Hwork1 {
 
-    public Hwork1(String s, int n) {
-        String b = s;
-        if (b.length() == 0) System.out.println("Strint shouldn't be empty");
+    public void everyNth(String s, int n) {
+       // String b = s;
+        StringBuilder sb = new StringBuilder();
+        if (s.length() == 0) System.out.println("Strint shouldn't be empty");
         else {
-            for (int i = 0; i < s.length(); i = i + n)
-                System.out.println(s.charAt(i));
+            for (int i = 0; i < s.length(); i = i + n) {
+                sb.append(s.charAt(i));
+            }
         }
+        System.out.println(sb);
     }
 }
 
--- src/Lesson02/Programm2.java
+++ src/Lesson02/Programm2.java
@@ -4,7 +4,9 @@ public class Programm2 {
     public static void main(String[] args) {
         //Person p1 = new Person("Bob", "Dilan",21);
         //Person p2 = new Person("Ivan", "Buinyi",12);
-        Hwork1  H1 = new Hwork1 ("Some text for home work test", 5);
+        //Hwork1  H1 = new Hwork1 ("Some text for home work test", 5);
+        Hwork1 hwork1 = new Hwork1();
+        hwork1.everyNth ("Some text for test", 2);
         //System.out.println(p1);
         //System.out.println(p2);
         }
